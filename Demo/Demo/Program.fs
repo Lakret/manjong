80a67606c87221efb0376690ecca43241738bcfe
Lakret <Rakuretu@gmail.com>
2012-10-23 23:41:03 +0600

    Annotations

--- Demo/Demo/Program.fs
+++ Demo/Demo/Program.fs
@@ -1,35 +1,44 @@
 ﻿open System
 
-let a = 300
-let b = 500.5
+//задание именованных значений
+let a = 300   //целое число, тип: integer
+let b = 500.5 //вещественное число, тип: float
+let str = "This is BlackJack!" //строка, тип: string
+let delimiter = ' ' //символ, тип: char
+let f = (fun x -> x * 2) //функция, тип: int -> int
 
-let str = "This is BlackJack!"
+let g x y = x * y //другая запись функции
+let g' = g 20     //частичное применение
 
-let delimiter = ' '
+Array.iter (printfn "-- %c") //функция из модуля Array
+<| str.ToCharArray()
 
-let f = (fun x -> x * 2) //int -> int
+[| 0..100 |] |> Array.min //минимальный элемент массива
+|> ignore //чтобы компилятор не ругался ^_^
 
-let g x y = x * y
-let g' = g 20
+let gen = new Random() //новый генератор случайных чисел
 
-Array.iter (printfn "-- %c") <| str.ToCharArray()
-
-[| 0..100 |] |> Array.min
-
-let gen = new Random()
-
-[| for _ in 0..100 -> gen.NextDouble() * 100.0 - 50.0 |]
+[| for _ in 0..100 -> gen.NextDouble() * 100.0 - 50.0 |] //массив случайных чисел, array comprehension
+//другой спосо найти минимальный элемент
 |> Array.fold (fun acc elem -> if elem < acc then elem else acc) Double.PositiveInfinity
+|> ignore
 
-let rec fold f init arr =
+//самодельный fold для списка
+let rec fold f init list = //rec потому что функция рекурсивная
+    match list with //pattern matching
+    | [] -> init //если пустой список — возвращаем аккумулятор
+    | x::xs -> fold f (f init x) xs //иначе применяем f к голове и тек. аккумулятору и продолжаем для хвоста
+
+[ for _ in 0..100 -> gen.NextDouble() * 100. - 30. ] //list comprehension
+|> fold (fun (min, max) elem -> (if elem < min then elem else min), (if elem > max then elem else max))
+         (Double.PositiveInfinity, Double.NegativeInfinity)
+||> printfn "Min: %f, Max: %f" // а про магический оператор ||> мы поговорим в след. среду :)
+
+//самодельный fold для массива
+let rec fold' f init arr =
     match arr with
     | [||] -> init
-    | _ -> fold f (f init arr.[0])  arr.[1..]
+    | _ -> fold' f (f init arr.[0]) arr.[1..]
 
-fold (+) 0 [|1..100|]
-
-
-
-
-//"hello world"
-//" hello Dmitriy"
+fold' (+) 0 [|1..100|] //сумма чисел через fold
+|> ignore
