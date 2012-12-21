ecb18cc71e40fd4a12e4de2608385fc0ed368cda
Lakret <Rakuretu@gmail.com>
2012-12-21 19:51:03 +0600

    Recursive bank

--- Demo/Bank/Program.fs
+++ Demo/Bank/Program.fs
@@ -7,8 +7,9 @@ type State =
     | User of string
 
 type Comand =
-    | Create of (string -> float)
+    | Create of (State -> string -> float)
     | Sub of (State -> float -> float)
+    | Help of (State -> (Comand * string) list)
 
 let db = new Dictionary<string, string option * float>()
 
@@ -16,27 +17,35 @@ let getAccountAmount =
     let rndGen = new Random()
     (fun() -> rndGen.NextDouble() * 1e9)
     
-let create name =  
-    let amount = getAccountAmount()
-    db.Add(name, (None, amount))
-    amount
+let create state name =  
+    match state with 
+    | Admin ->
+        let amount = getAccountAmount()
+        db.Add(name, (None, amount))
+        printfn "User [%s] with amount %.2f has been created" name amount
+        amount
+    | User _ -> failwith "User cannot create another user"
 
 let sub state amount =
     match state with 
     | Admin -> failwith "Admin have no account"
     | User name -> 
         if db.ContainsKey name then            
-            let newAmount = snd db.[name] - amount
+            let currentAmount = snd db.[name]
+            printfn "Currently you have $%f and want to withdrawl $%f" currentAmount amount
+            let newAmount = currentAmount - amount
             db.[name] <- (fst db.[name], newAmount)
+            printfn "Succesful, new amount is $%F" newAmount
             newAmount 
         else failwith <| sprintf "There's no user with name %s" name   
 
-let describe state = 
+let rec describe state = 
+    let helpDescription = Help describe, "Describe Available commands"
     match state with
-    | Admin -> [Create create , "New User"]
-    | User _ -> [Sub sub , "Withdrawl the given amount of money from the user's account"]
+    | Admin -> [Create create , "New User"; helpDescription]
+    | User _ -> [Sub sub , "Withdrawl the given amount of money from the user's account"; helpDescription]
 
-let login (_state : State) =
+let login () =
     let requestPassword correctPassword succesState =
         printfn "Enter your password"
         let userpassword = Console.ReadLine()
@@ -57,8 +66,38 @@ let login (_state : State) =
             db.[name] <- (Some password, snd db.[name])
             User name
 
+let logout () = login ()
+
+let splitWithConversion (input : string) commandName f =
+    let splitted = input.Split([|' '; '\t'|], StringSplitOptions.RemoveEmptyEntries)
+    if (splitted.Length = 2) && (splitted.[0] = commandName) then  
+        let (success, value) = f splitted.[1]
+        if success then Some(splitted.[0], value) else None
+    else None
+
+let (| StringWithAmount |_|) (input : string) = splitWithConversion input "sub" (Double.TryParse) 
+let (| StringWithName |_|) (input : string) = splitWithConversion input "create" (fun x -> true, x) 
+
+let executor state (input : string) =
+    match input with
+    | "help" -> 
+        printfn "Available commands:"
+        describe state |> List.iter (printfn "%A")
+        state
+    | "logout" -> logout ()
+    | StringWithAmount(command, amount) -> 
+        sub state amount |> ignore
+        state
+    | StringWithName(command, name) -> 
+        create state name |> ignore  
+        state
+    | _ -> failwith "Unknown command"
+
+let rec loopWithState f startState = loopWithState f <| f startState 
+
 [<EntryPoint>]
 let main(_) =
-    printfn "Available commands: %A" <| describe Admin
-    login Admin |> printfn "Current state %A"
+    let startState = login () 
+    printfn "Current state %A" startState
+    loopWithState (fun state -> executor state (Console.ReadLine())) startState
     0
