50185ad5615d1e0daaaf15eb0b5488b1c3dc66ba
Dmitry Slutsky <Rakuretu@gmail.com>
2013-03-15 19:37:34 +0600

    Update knn.fs

--- knn.fs
+++ knn.fs
@@ -19,3 +19,24 @@ let kNN k metric world test =
     closest.[..(k-1)]
     |> Seq.countBy (fun x -> x.clazz) 
     |> Seq.maxBy fst
+    
+let world =
+    [| 
+        {coords = [|1.; 3.|]; clazz = "red"}
+        {coords = [|2.; 3.|]; clazz = "red"}
+        {coords = [|2.; 2.|]; clazz = "red"}
+        {coords = [|2.; 1.|]; clazz = "red"}
+        {coords = [|4.; 1.|]; clazz = "red"}
+        {coords = [|0.; 0.|]; clazz = "green"}
+        {coords = [|0.; 1.|]; clazz = "green"}
+        {coords = [|1.; 1.|]; clazz = "green"}  
+        {coords = [|1.; 0.|]; clazz = "green"} 
+        {coords = [|2.; 0.|]; clazz = "green"}  
+        {coords = [|3.; 4.|]; clazz = "green"} 
+        {coords = [|4.; 4.|]; clazz = "green"}  
+        {coords = [|4.; 3.|]; clazz = "green"}
+    |]
+    
+let test = [|3.; 1.|]
+
+kNN 3 euclideanDistance world test
